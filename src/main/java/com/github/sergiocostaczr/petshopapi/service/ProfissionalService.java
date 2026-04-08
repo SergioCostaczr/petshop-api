f8c8ced0fe53aec23592bd6c520f3ceac6b0a3d4
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-08 00:53:22 -0300

    feat: metodos crud

--- src/main/java/com/github/sergiocostaczr/petshopapi/service/ProfissionalService.java
+++ src/main/java/com/github/sergiocostaczr/petshopapi/service/ProfissionalService.java
@@ -4,13 +4,51 @@ import com.github.sergiocostaczr.petshopapi.dto.ProfissionalRequestDTO;
 import com.github.sergiocostaczr.petshopapi.dto.ProfissionalResponseDTO;
 import com.github.sergiocostaczr.petshopapi.entity.Profissional;
 import com.github.sergiocostaczr.petshopapi.repository.ProfissionalRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProfissionalService {
     @Autowired
     private ProfissionalRepository repository;
+
+    public ProfissionalResponseDTO salvar(ProfissionalRequestDTO dto){
+        Profissional entidade = new Profissional();
+        dtoParaEntidade(dto,entidade);
+        Profissional entidadeSalva = repository.save(entidade);
+        return entidadeParaResponseDto(entidadeSalva);
+    }
+
+    public ProfissionalResponseDTO buscarPorId(Long id){
+        Profissional profissional = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Profissional não entcontrado"));
+
+        return entidadeParaResponseDto(profissional);
+    }
+
+    public ProfissionalResponseDTO atualizarPorId(Long id, ProfissionalRequestDTO dto){
+        Profissional entidade = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Profissional não entcontrado"));
+        dtoParaEntidade(dto,entidade);
+
+        Profissional entidadeAtualizada = repository.save(entidade);
+
+        return entidadeParaResponseDto(entidadeAtualizada);
+
+    }
+
+    public List<ProfissionalResponseDTO> listar(){
+        List<Profissional> lista = repository.findAll();
+
+        return lista.stream().map(x -> entidadeParaResponseDto(x)).toList();
+
+    }
+
+    public void deletarPorId(Long id){
+        Profissional entidade = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Profissional não entcontrado"));
+        repository.deleteById(id);
+    }
 
     private void dtoParaEntidade(ProfissionalRequestDTO dto, Profissional profissional){
         profissional.setNome(dto.nome());
