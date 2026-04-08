ac9c28977131336e3bf8ee6911fcbdf2e58dc86c
juliaalvino123 <juliaalvino123@gmail.com>
2026-04-07 23:42:48 -0300

    Create Cliente.java

---
+++ src/main/java/com/github/sergiocostaczr/petshopapi/entity/Cliente.java
@@ -0,0 +1,31 @@
+package com.github.sergiocostaczr.petshopapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "cliente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @OneToMany(mappedBy = "clinte")
+    @Setter(AccessLevel.NONE)
+    List<Pet> petLista;
+}
