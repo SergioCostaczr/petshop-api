d238306bb6c6279f85011ab123186ef3db95fef1
juliaalvino123 <juliaalvino123@gmail.com>
2026-04-07 23:47:21 -0300

    Create ProfissionalRepository.java

---
+++ src/main/java/com/github/sergiocostaczr/petshopapi/entity/ProfissionalRepository.java
@@ -0,0 +1,7 @@
+package com.github.sergiocostaczr.petshopapi.repository;
+
+import com.github.sergiocostaczr.petshopapi.entity.Profissional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
+}
