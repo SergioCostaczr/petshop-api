c555a5b1d335a73d1b7fe8a73d9e4a057433f9b4
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-08 22:57:29 -0300

    feat: Profissional request & response DTOs @Schemas annotation swagger

--- src/main/java/com/github/sergiocostaczr/petshopapi/dto/ProfissionalResponseDTO.java
+++ src/main/java/com/github/sergiocostaczr/petshopapi/dto/ProfissionalResponseDTO.java
@@ -1,17 +1,20 @@
 package com.github.sergiocostaczr.petshopapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record ProfissionalResponseDTO(
+        @Schema(description = "ID do profissional", example = "1")
         Long id,
-
+        @Schema(description = "Nome do profissional", example = "Maria Souza")
         String nome,
-
+        @Schema(description = "Função do profissional", example = "Veterinário")
         String funcao,
-
-        BigDecimal Salario,
-
+        @Schema(description = "Salário do profissional", example = "3500.00")
+        BigDecimal salario,
+        @Schema(description = "Data de cadastro do profissional", example = "2024-01-15T10:30:00")
         LocalDateTime dataCadastro
 
 ) {
