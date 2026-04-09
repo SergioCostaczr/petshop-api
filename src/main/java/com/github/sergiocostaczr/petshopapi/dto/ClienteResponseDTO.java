0247251d063dd9c6ef8feba1a7d8a29e96a41090
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-08 22:57:36 -0300

    feat: Cliente request & response DTOs @Schemas annotation swagger

--- src/main/java/com/github/sergiocostaczr/petshopapi/dto/ClienteResponseDTO.java
+++ src/main/java/com/github/sergiocostaczr/petshopapi/dto/ClienteResponseDTO.java
@@ -1,14 +1,20 @@
 package com.github.sergiocostaczr.petshopapi.dto;
 
 import com.github.sergiocostaczr.petshopapi.entity.Pet;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record ClienteResponseDTO(
+        @Schema(description = "ID do cliente", example = "1")
         Long id,
+        @Schema(description = "Nome do cliente", example = "João Silva")
         String nome,
+        @Schema(description = "Telefone do cliente", example = "(85) 99999-9999")
         String telefone,
+        @Schema(description = "Endereço do cliente", example = "Rua das Flores, 123, Fortaleza - CE")
         String endereco,
+        @Schema(description = "Lista de pets do cliente")
         List<PetResponseDTO> petLista
 ) {
 }
