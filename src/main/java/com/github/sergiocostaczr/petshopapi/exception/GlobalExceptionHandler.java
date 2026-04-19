23131eae39aa5935c3065bea5a1ac506abc6b0f1
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-19 01:26:44 -0300

    GlobalExceptionHandler

---
+++ src/main/java/com/github/sergiocostaczr/petshopapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.github.sergiocostaczr.petshopapi.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
