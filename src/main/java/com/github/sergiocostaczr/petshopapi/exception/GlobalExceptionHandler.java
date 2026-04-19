2bf30bb78330a63983d53a13cdd2058ee2b5e91c
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-19 01:38:51 -0300

    handle MethodArgumentNotValidException

--- src/main/java/com/github/sergiocostaczr/petshopapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/sergiocostaczr/petshopapi/exception/GlobalExceptionHandler.java
@@ -4,8 +4,11 @@ import jakarta.persistence.EntityNotFoundException;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +16,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> handleNotFound(EntityNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException e){
+        List<String> erros = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(erro-> erro.getField() + ": " + erro.getDefaultMessage())
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
     }
 }
