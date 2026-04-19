7b28e451d4fbd10bb09891d0d1daac23e589fb43
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-19 01:27:22 -0300

    handle EntityNotFoundException

--- src/main/java/com/github/sergiocostaczr/petshopapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/sergiocostaczr/petshopapi/exception/GlobalExceptionHandler.java
@@ -10,5 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(EntityNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 }
