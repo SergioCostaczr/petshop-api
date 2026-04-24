bb043510cb125fd8cee0b94c6362fcf0fdb84052
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-04-24 16:04:34 -0300

    feat: Email duplicado exception

---
+++ src/main/java/com/github/sergiocostaczr/petshopapi/exception/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.github.sergiocostaczr.petshopapi.exception;
+
+public class EmailJaCadastradoException extends RuntimeException {
+    public EmailJaCadastradoException(String message) {
+        super(message);
+    }
+}
